a5cedf49031da782bd7e7d1610e9e76034837add
Raving Genius <rg+code@ravinggenius.com>
2010-05-06 20:23:23 -0400

    Updated API to use MongoMapper

--- models/list.rb
+++ models/list.rb
@@ -1,25 +1,5 @@
-class List
-  include DataMapper::Resource
-  include NodeExtension
+class List < Node
+  key :items, Array
 
-  GLUE = "\n"
-
-  property :id, Serial
-  property :data, Text, :required => true, :lazy => false, :accessor => :protected
-
-  before :save do
-    @items ||= []
-    self.data = @items.join GLUE
-  end
-
-  after :save, :set_id
-
-  def items
-    self.data ||= ''
-    @items ||= self.data.split GLUE
-  end
-
-  def items= list
-    @items = list
-  end
+  validates_presence_of :items
 end
